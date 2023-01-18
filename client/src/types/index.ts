76c04355db432450505264eec0b1d65ab784bf76
양현석 <yyh7654@gmail.com>
2023-01-18 13:41:51 +0900

    :sparkles: Add types based on json data in myDB

---
+++ client/src/types/index.ts
@@ -0,0 +1,32 @@
+export interface PageInfo {
+    title: string;
+    href: string;
+}
+
+export interface Contents {
+    index: number;
+    category: string;
+    link: string;
+    origin: string;
+    isBookMarked: boolean;
+}
+
+export enum Category {
+    popularity = "popularity",
+    news = "news",
+    sports = "sports",
+    webtoon = "webtoon",
+    dict = "dict",
+}
+
+export enum ApiStatus {
+    LOADING = "LOADING",
+    DONE = "DONE",
+    FAIL = "FAIL",
+}
+
+export interface Action<T = unknown> {
+    type: string;
+    data?: T;
+    error?: string;
+}
